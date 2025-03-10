7ecb558f7b66f28523844dcbc71f0169f29dcf4e
Raima <raimagull87@gmail.com>
2025-03-10 16:15:40 -0700

    Updates all files

--- detail.js
+++ detail.js
@@ -1,5 +1,12 @@
 let searchParams=new URLSearchParams(window.location.search)
 let paramValue=searchParams.get("id")
+let detailBox=document.getElementById("detail-box")
+let img=document.getElementById("img")
+let title=document.getElementById("title")
+let desc=document.getElementById("desc")
+let price=document.getElementById("price")
+let btn=document.getElementById("btn")
+let category=document.getElementById("category")
 // console.log(paramValue)
 
 if(!paramValue||searchParams.size>1){
@@ -12,6 +19,12 @@ fetch(`https://fakestoreapi.com/products/${paramValue}`)
 })
 .then((res)=>{
     console.log(res)
+    title.innerHTML=res.title
+    desc.innerHTML=res.description
+    price.innerHTML=`Price: Rs ${res.price}`
+    category.innerHTML=`Category:  ${res.category}`
+    img.src=res.image
+    btn.innerHTML="Add To cart"
 })
 .catch((err)=>{
  alert("This product id is not available");
